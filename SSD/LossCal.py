234e3c62d77bd7bb5ba71fdb0e6f97dbc6546ffa
wudan <wudan@gzseeing.com>
2018-10-17 17:28:37 +0800

    1.增加loss文件

---
+++ SSD/LossCal.py
@@ -0,0 +1,15 @@
+import tensorflow as tf
+
+def lossMultiLayers(MultiLayers):
+    predictions = []
+    logits = []
+    for i,lays in enumerate(MultiLayers):
+        p,l = lossSingleLay(lays)
+        predictions.append(p)
+        logits.append(l)
+    return predictions, logits
+
+def lossSingleLay(Inputs):
+    p = []
+    l = []
+    return p,l
